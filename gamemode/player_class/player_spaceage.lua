6cfd729b296da4d488abbc78bf59bcd214ac4a97
Michael Johnson <michael@johnson.computer>
2016-03-14 00:13:09 -0400

    Handle player's initial spawn correctly in regards to current planet

--- gamemode/player_class/player_spaceage.lua
+++ gamemode/player_class/player_spaceage.lua
@@ -10,6 +10,10 @@ function PLAYER:SetupDataTables()
 
 	self.Player:NetworkVar("String", 0, "AreaName")
 	self.Player:NetworkVar("Bool", 0, "InSpace")
+
+	if SERVER then
+		self.Player:DataTablesInitialized()
+	end
 end
 
 player_manager.RegisterClass("player_spaceage", PLAYER, "player_sandbox")
