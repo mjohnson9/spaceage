61a369c592c8e0f38102bfcb216492361dc2c111
Michael Johnson <michael@johnson.computer>
2016-03-14 00:13:09 -0400

    Handle player's initial spawn correctly in regards to current planet

--- gamemode/metatables/sv_player.lua
+++ gamemode/metatables/sv_player.lua
@@ -1,7 +1,6 @@
 -- Copyright (C) Charles Leasure, Mark Dietzer, and Michael Johnson d.b.a SpaceAge - All Rights Reserved
 -- See LICENSE file for more information.
 
-include("plifo.lua")
 include("../utilities/planet_util.lua")
 include("../utilities/plifo.lua")
 
@@ -9,6 +8,15 @@ local PLAYER = FindMetaTable("Player")
 
 function PLAYER:InitializeSpaceAge()
 	self.planets = plifo.new(planet_util.compare)
+end
+
+-- this hook is to be called after DataTables are initialized
+-- behavior when called manually is undefined
+function PLAYER:DataTablesInitialized()
+	if self.planetDirty then
+		self:ApplyPlanet(self.planets[#self.planets])
+		self.planetDirty = nil
+	end
 end
 
 function PLAYER:EnteredPlanet(planet)
@@ -24,6 +32,13 @@ function PLAYER:ExitedPlanet(planet)
 end
 
 function PLAYER:ApplyPlanet(planet)
+	if not self.SetAreaName then
+		-- the player just joined and hasn't fully initialized yet
+		-- mark them dirty and PLAYER:SpaceAgeSpawned will try again after they've initialized
+		self.planetDirty = true
+		return
+	end
+
 	if planet == nil then
 		self:SetGravity(0.00001)
 		self:SetAreaName("Space")
