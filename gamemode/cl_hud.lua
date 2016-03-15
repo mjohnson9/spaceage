95a709451df9e977158bcba296f565934c911779
Michael Johnson <michael@johnson.computer>
2016-03-15 13:06:42 -0400

    Patch up HUD

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,9 +1,17 @@
 -- Copyright (C) Charles Leasure, Mark Dietzer, and Michael Johnson d.b.a SpaceAge - All Rights Reserved
 -- See LICENSE file for more information.
-	
+
 -- Cache the hud backgrounds to prevent call them every frame
-self.HudBackground = Material("spaceage/hud_background.png")
-self.HudDrain = Material("spaceage/hud_drain.png")
+local hudBackground = Material("spaceage/hud_background.png")
+local hudDrain = Material("spaceage/hud_drain.png")
+local ScrH = ScrH
+
+local colorEnvironment = Color(255, 255, 255)
+local colorHealth = Color(214, 0, 3)
+local colorEnergy = Color(71, 230, 0)
+local colorOxygen = Color(255, 255, 255)
+local colorCoolant = Color(0, 180, 231)
+local colorBlack = Color(0, 0, 0)
 
 function GM:HUDPaint()
 	self.BaseClass.HUDPaint(self)
@@ -14,63 +22,55 @@ function GM:HUDPaint()
 	local playerEnergy = self.LocalPlayer:Armor()
 	local playerOxygen = 60
 	local playerCoolant = 20
-	local screenWidth  = ScrW()
 	local screenHeight = ScrH()
 
 	-- Local Functions
-	local playerHealthAdjust = (62) * ((100 - playerHealth)/100)
-	local playerEnergyAdjust = (62) * ((100 - playerEnergy)/100)
-	local playerOxygenAdjust = (62) * ((100 - playerOxygen)/100)
-	local playerCoolantAdjust = (62) * ((100 - playerCoolant)/100)
+	local playerHealthAdjust = (62) * ((100 - playerHealth) / 100)
+	local playerEnergyAdjust = (62) * ((100 - playerEnergy) / 100)
+	local playerOxygenAdjust = (62) * ((100 - playerOxygen) / 100)
+	local playerCoolantAdjust = (62) * ((100 - playerCoolant) / 100)
 
 	-- Draw Base HUD
 	surface.SetDrawColor(255, 255, 255, 255)
-	surface.SetMaterial(self.HudBackground)
-	surface.DrawTexturedRect(5, screenHeight -122, 306, 111)
+	surface.SetMaterial(hudBackground)
+	surface.DrawTexturedRect(5, screenHeight - 122, 306, 111)
 
 	-- Draw Environment Name
-	draw.SimpleTextOutlined((playerArea), "DermaDefaultBold", 70, screenHeight - 113, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))
-	
+	draw.SimpleTextOutlined(playerArea, "DermaDefaultBold", 70, screenHeight - 113, colorEnvironment, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, colorBlack)
+
 	-- Draw Health Bar
 	surface.SetDrawColor(255, 255, 255, 255)
-	surface.SetMaterial(self.HudDrain)
+	surface.SetMaterial(hudDrain)
 	surface.DrawTexturedRect(138, screenHeight - 94, 27, playerHealthAdjust)
 	-- Draw Health Percent
-	draw.SimpleTextOutlined((playerHealth), "DermaDefaultBold", 151, screenHeight - 31, Color(214, 0, 3), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))
+	draw.SimpleTextOutlined(playerHealth, "DermaDefaultBold", 151, screenHeight - 31, colorHealth, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, colorBlack)
 
 	-- Draw Energy Bar
 	surface.SetDrawColor(255, 255, 255, 255)
-	surface.SetMaterial(self.HudDrain)
+	surface.SetMaterial(hudDrain)
 	surface.DrawTexturedRect(184, screenHeight - 94, 27, playerEnergyAdjust)
 	-- Draw Energy Percent
-	draw.SimpleTextOutlined((playerEnergy), "DermaDefaultBold", 197, screenHeight - 31, Color(71, 230, 0), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))
+	draw.SimpleTextOutlined(playerEnergy, "DermaDefaultBold", 197, screenHeight - 31, colorEnergy, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, colorBlack)
 
 	-- Draw Oxygen Bar
 	surface.SetDrawColor(255, 255, 255, 255)
-	surface.SetMaterial(self.HudDrain)
+	surface.SetMaterial(hudDrain)
 	surface.DrawTexturedRect(230, screenHeight - 94, 27, playerOxygenAdjust)
 	-- Draw Oxygen Percent
-	draw.SimpleTextOutlined((playerOxygen), "DermaDefaultBold", 243, screenHeight - 31, Color(255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))
+	draw.SimpleTextOutlined(playerOxygen, "DermaDefaultBold", 243, screenHeight - 31, colorOxygen, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, colorBlack)
 
 	-- Draw Coolant Bar
 	surface.SetDrawColor(255, 255, 255, 255)
-	surface.SetMaterial(self.HudDrain)
+	surface.SetMaterial(hudDrain)
 	surface.DrawTexturedRect(276, screenHeight - 94, 27, playerCoolantAdjust)
 	-- Draw Coolant Percent
-	draw.SimpleTextOutlined((playerCoolant), "DermaDefaultBold", 290, screenHeight - 31, Color(0, 180, 231), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0))
-
-
-
+	draw.SimpleTextOutlined(playerCoolant, "DermaDefaultBold", 290, screenHeight - 31, colorCoolant, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, colorBlack)
 end
 
 function GM:HUDShouldDraw(name)
-	if	name == 'CHudHealth' ||
-		name == 'CHudBattery' ||
-		name == 'CHudAmmo' ||
-		name == 'CHudSecondaryAmmo'
-	then
+	if	name == "CHudHealth" or name == "CHudBattery" or name == "CHudAmmo" or name == "CHudSecondaryAmmo" then
 		return false
 	end
-	
+
 	return true
 end
