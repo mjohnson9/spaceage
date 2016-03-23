b45ed42df1a4265bdc7e4def834301f67eb8e902
Michael Johnson <michael@johnson.computer>
2016-03-23 12:58:15 -0400

    [BROKEN] Attempt at a custom package.loader -- garry's mod ignores these

--- gamemode/modules/sv_planet_util.lua
+++ gamemode/modules/sv_planet_util.lua
@@ -1,8 +1,10 @@
 module("planet_util")
 
 function compare(planet1, planet2)
-	if planet1:GetPlanetPriority() < planet2:GetPlanetPriority() then
-		return true
+	local priority1 = planet1:GetPlanetPriority()
+	local priority2 = planet2:GetPlanetPriority()
+	if priority1 ~= priority2 then
+		return priority1 < priority2
 	end
 
 	return planet1:EntIndex() < planet2:EntIndex()
