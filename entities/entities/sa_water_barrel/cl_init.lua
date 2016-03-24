7da7a476587c6d7c3aaab7c6c30552345b76d3aa
icew01f <icewolf575@gmail.com>
2016-03-24 11:12:55 -0700

    Added in-game UI to the sa_water_barrel

--- entities/entities/sa_water_barrel/cl_init.lua
+++ entities/entities/sa_water_barrel/cl_init.lua
@@ -1,30 +1,40 @@
 include("info.lua")
 include("shared.lua")
 
-ENT.RenderPosition = Vector(-9, 9.1, 26)
+ENT.RenderPosition = Vector(20, -11.6, 26)
 
 function ENT:GetPanelPos()
 	local pos = self:LocalToWorld(self.RenderPosition)
 	local ang = self:GetAngles()
-	return pos, ang, 0.1
+	return pos, Angle(ang[1], ang[2] + 90, ang[3] + 90), 0.13
 end
 
 function ENT:CreatePanel()
-	local primaryPanel = vgui.Create("DProgress")
 
-	primaryPanel:SetPos(0, 0)
-	primaryPanel:SetSize(182, 182)
-	primaryPanel:SetFraction(0)
+	local displayContainer = vgui.Create("DPanel")
+	displayContainer:SetSize(182, 182)
+	displayContainer:SetPos(0, 0)
+	displayContainer:SetPaintBackground(false)
 
-	--[[local DLabel = vgui.Create("DLabel", primaryPanel)
-	DLabel:SetPos(10, 10)
-	DLabel:SetText("Hello, world!")
-	DLabel:SizeToContents()
-	DLabel:SetDark(1)]]
+	local waterLabel = vgui.Create("DPanel", displayContainer)
+	waterLabel.Paint = function()
+		draw.SimpleTextOutlined("Water", "DermaDefault", 91, 20, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0, 255))
+	end
 
-	return primaryPanel
-end
+	local waterCountLabel = vgui.Create("DPanel", displayContainer)
+	waterCountLabel.Paint = function()
+		draw.SimpleTextOutlined(self:GetWater(), "DermaDefault", 91, 35, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0, 255))
+	end
 
-function ENT:UpdatePanel()
-	self.statusPanel:SetFraction(self:GetWater() / self.ResourceStorage.Water)
+	local waterStorageLabel = vgui.Create("DPanel", displayContainer)
+	waterStorageLabel.Paint = function()
+		draw.SimpleTextOutlined("Maxium Storage", "DermaDefault", 91, 115, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0, 255))
+	end
+
+	local waterStorageCountLabel = vgui.Create("DPanel", displayContainer)
+	waterStorageCountLabel.Paint = function()
+		draw.SimpleTextOutlined(self.ResourceStorage.Water, "DermaDefault", 91, 130, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, 1, Color(0, 0, 0, 255))
+	end
+
+	return displayContainer
 end
