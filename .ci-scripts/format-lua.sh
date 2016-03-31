eac7c4d5e07567838975cfec3e7ae308dac39899
Michael Johnson <michael@johnson.computer>
2016-03-30 23:05:28 -0400

    CI: update from spaceage/spacebuild

---
+++ .ci-scripts/format-lua.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TEMP_FILE="$(mktemp)"
+function finish {
+	# Remove our temporary file
+	rm -f -- "${TEMP_FILE}"
+}
+trap finish EXIT
+
+RETURN_STATUS=0
+
+for filepath in $(find . -type f -iname '*.lua' -not -iwholename '*.git*'); do
+	.bin/glualint --config .glualint.json --pretty-print < "${filepath}" > "${TEMP_FILE}"
+	status=$?
+	if [ $status -eq 0 ]; then
+		cat "${TEMP_FILE}" > "${filepath}"
+		echo "Formatted ${filepath}"
+	else
+		echo "Failed to format ${filepath}"
+		RETURN_STATUS=1
+	fi
+done
+
+exit $RETURN_STATUS
