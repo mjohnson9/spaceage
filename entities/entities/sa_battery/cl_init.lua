858b1cff2283620b92c9dff912450d41224b81ee
Michael Johnson <michael@johnson.computer>
2016-03-31 00:35:13 -0400

    Round numbers displayed on storage devices

--- entities/entities/sa_battery/cl_init.lua
+++ entities/entities/sa_battery/cl_init.lua
@@ -40,6 +40,8 @@ function ENT:CreatePanel()
 	return displayContainer
 end
 
+local mathRound = math.Round
+
 function ENT:UpdatePanel()
-	self.energyCountLabel:SetText(self:GetEnergy())
+	self.energyCountLabel:SetText(mathRound(self:GetEnergy()))
 end
