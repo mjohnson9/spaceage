edeb11a10bbb0533afe259330eda66c64c2cde48
Michael Johnson <michael@johnson.computer>
2016-03-30 23:24:36 -0400

    Clear hud/cl_init of glualint issues

--- gamemode/extensions/hud/cl_init.lua
+++ gamemode/extensions/hud/cl_init.lua
@@ -2,19 +2,19 @@
 -- See LICENSE file for more information.
 
 -- Cache the hud backgrounds to prevent call them every frame
-local hudDrain = Material("spaceage/hud_drain.png")
+local hudDrain = "spaceage/hud_drain.png"
 
 -- localize functions to prevent global table lookups every frame
 local ScrH = ScrH
 local mathMax = math.max
 local mathMin = math.min
+local mathRemap = math.Remap
 
 local colorEnvironment = Color(255, 255, 255)
 local colorHealth = Color(214, 0, 3)
 local colorEnergy = Color(71, 230, 0)
 local colorOxygen = Color(255, 255, 255)
 local colorCoolant = Color(0, 180, 231)
-local colorBlack = Color(0, 0, 0)
 
 local localPlayer = LocalPlayer() -- save for later so that we aren't calling LocalPlayer every frame
 local screenHeight = ScrH()
@@ -33,36 +33,54 @@ hudContainerBackground:SetImage("spaceage/hud_background.png")
 -- Create Environment Label
 local environmentLabel = vgui.Create("DLabel", hudContainer)
 environmentLabel:SetPos(34, 5)
+environmentLabel:SetContentAlignment(5)
+environmentLabel:SetColor(colorEnvironment)
 
 -- Create Resource Labels
 local healthLabel = vgui.Create( "DLabel", hudContainer )
-healthLabel:SetPos( 115, 88 )
+healthLabel:SetPos(115, 88)
+healthLabel:SetFont("DermaDefaultBold")
+healthLabel:SetContentAlignment(5)
+healthLabel:SetColor(colorHealth)
 
 local energyLabel = vgui.Create( "DLabel", hudContainer )
-energyLabel:SetPos( 161, 88 )
+energyLabel:SetPos(161, 88)
+energyLabel:SetFont("DermaDefaultBold")
+energyLabel:SetContentAlignment(5)
+energyLabel:SetColor(colorEnergy)
 
 local oxygenLabel = vgui.Create( "DLabel", hudContainer )
-oxygenLabel:SetPos( 207, 88 )
+oxygenLabel:SetPos(207, 88)
+oxygenLabel:SetFont("DermaDefaultBold")
+oxygenLabel:SetContentAlignment(5)
+oxygenLabel:SetColor(colorOxygen)
 
 local coolantLabel = vgui.Create( "DLabel", hudContainer )
-coolantLabel:SetPos( 253, 88 )
+coolantLabel:SetPos(253, 88)
+coolantLabel:SetFont("DermaDefaultBold")
+coolantLabel:SetContentAlignment(5)
+coolantLabel:SetColor(colorCoolant)
 
 -- Create Resource Bars
-local healthBar = vgui.Create("DPanel", hudContainer)
+local healthBar = vgui.Create("DImage", hudContainer)
 healthBar:SetPos(133, 28)
 healthBar:SetSize(27, 0)
+healthBar:SetImage(hudDrain)
 
-local energyBar = vgui.Create("DPanel", hudContainer)
+local energyBar = vgui.Create("DImage", hudContainer)
 energyBar:SetPos(179, 28)
 energyBar:SetSize(27, 0)
+energyBar:SetImage(hudDrain)
 
-local oxygenBar = vgui.Create("DPanel", hudContainer)
+local oxygenBar = vgui.Create("DImage", hudContainer)
 oxygenBar:SetPos(225, 28)
 oxygenBar:SetSize(27, 0)
+oxygenBar:SetImage(hudDrain)
 
-local coolantBar = vgui.Create("DPanel", hudContainer)
+local coolantBar = vgui.Create("DImage", hudContainer)
 coolantBar:SetPos(271, 28)
 coolantBar:SetSize(27, 0)
+coolantBar:SetImage(hudDrain)
 
 function HOOKS:InitPostEntity()
 	localPlayer = LocalPlayer()
@@ -81,60 +99,25 @@ function HOOKS:HUDPaint()
 	local playerCoolantPercent = mathMin(playerCoolantText, 100)
 
 	-- Local Functions
-	local playerHealthAdjust  = (62) * ((100 - playerHealthPercent)  / 100)
-	local playerEnergyAdjust  = (62) * ((100 - playerEnergyPercent)  / 100)
-	local playerOxygenAdjust  = (62) * ((100 - playerOxygenPercent)  / 100)
-	local playerCoolantAdjust = (62) * ((100 - playerCoolantPercent) / 100)
+	local playerHealthAdjust  = mathRemap(playerHealthPercent, 0, 100, 62, 0)
+	local playerEnergyAdjust  = mathRemap(playerEnergyPercent, 0, 100, 62, 0)
+	local playerOxygenAdjust  = mathRemap(playerOxygenPercent, 0, 100, 62, 0)
+	local playerCoolantAdjust = mathRemap(playerCoolantPercent, 0, 100, 62, 0)
 
 	-- Update EnvironmentLabel
 	environmentLabel:SetText(playerArea)
-	environmentLabel:SetContentAlignment(5)
 
 	-- Update Resource Labels
 	healthLabel:SetText(playerHealthPercent)
-	healthLabel:SetFont("DermaDefaultBold")
-	healthLabel:SetContentAlignment(5)
-	healthLabel:SetColor(colorHealth)
-
 	energyLabel:SetText(playerEnergyPercent)
-	energyLabel:SetFont("DermaDefaultBold")
-	energyLabel:SetContentAlignment(5)
-	energyLabel:SetColor(colorEnergy)
-
 	oxygenLabel:SetText(playerOxygenPercent)
-	oxygenLabel:SetFont("DermaDefaultBold")
-	oxygenLabel:SetContentAlignment(5)
-	oxygenLabel:SetColor(colorOxygen)
-
 	coolantLabel:SetText(playerCoolantPercent)
-	coolantLabel:SetFont("DermaDefaultBold")
-	coolantLabel:SetContentAlignment(5)
-	coolantLabel:SetColor(colorCoolant)
 
 	-- Update Resource Bars
 	healthBar:SetSize(27, playerHealthAdjust)
-	healthBar.Paint = function()
-		surface.SetMaterial(hudDrain)
-		healthBar:DrawTexturedRect()
-	end
-
 	energyBar:SetSize(27, playerEnergyAdjust)
-	energyBar.Paint = function()
-		surface.SetMaterial(hudDrain)
-		energyBar:DrawTexturedRect()
-	end
-
 	oxygenBar:SetSize(27, playerOxygenAdjust)
-	oxygenBar.Paint = function()
-		surface.SetMaterial(hudDrain)
-		oxygenBar:DrawTexturedRect()
-	end
-
 	coolantBar:SetSize(27, playerCoolantAdjust)
-	coolantBar.Paint = function()
-		surface.SetMaterial(hudDrain)
-		oxygenBar:DrawTexturedRect()
-	end
 end
 
 function HOOKS:HUDShouldDraw(name)
