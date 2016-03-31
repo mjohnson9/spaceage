d3493362e78f2046d88d2f18acc62b2fe16203f6
Michael Johnson <michael@johnson.computer>
2016-03-30 23:05:28 -0400

    CI: update from spaceage/spacebuild

---
+++ .ci-scripts/workshop-package.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+# Base directory
+echo "-- Creating content directory"
+mkdir -p workshop-content
+
+# LICENSE
+echo "-- Copying license..."
+cp -auv LICENSE workshop-content/
+
+# Maps
+echo "-- Copying maps..."
+mkdir -p workshop-content/maps
+cp -auv content/maps/*.bsp workshop-content/maps/
+
+# Materials
+echo "-- Copying materials..."
+mkdir -p workshop-content/materials
+cp -rauv content/materials/. workshop-content/materials/
+
+# Models
+echo "-- Copying models..."
+mkdir -p workshop-content/models
+cp -rauv content/models/. workshop-content/models/
+
+# Particles
+echo "-- Copying particles..."
+mkdir -p workshop-content/particles
+cp -rauv content/particles/. workshop-content/particles/
+
+# Sound
+echo "-- Copying sounds..."
+mkdir -p workshop-content/sound
+cp -rauv content/sound/. workshop-content/sound/
+
+# Remove Git files
+echo "-- Removing Git leftovers"
+find workshop-content -iname '.git*' -delete
