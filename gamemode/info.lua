d2f2400c9954dfad17345396774f6a5f567d79bb
Michael Johnson <michael@johnson.computer>
2016-04-01 11:54:07 -0400

    Parse and store version information

--- gamemode/info.lua
+++ gamemode/info.lua
@@ -5,3 +5,9 @@ DeriveGamemode("sandbox")
 
 GM.Name = "SpaceAge"
 GM.VCSInfo = "$Id$"
+if #GM.VCSInfo == 47 then -- VCSInfo should be 47 characters long if it was substituted properly
+	GM.Version = string.sub(GM.VCSInfo, -42, -2)
+else
+	GM.Version = "Unknown"
+end
+MsgC(Color(0, 183, 235), "Loading SpaceAge version " .. GM.Version .. "\n")
